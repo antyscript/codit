1ea2941ea90487846929dc7769e2dea321e3a19a
antyscript <antyscript@gmail.com>
2025-08-13 20:00:32 +0100

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,33 @@
 const mongoose = require('mongoose');
 const express = require('express');
+require('dotenv').config();
 const app = express();
 app.use(express.json());
-const Postschema = require('./schema');
+const { Schema } = mongoose;
+const cors = require('cors');
+app.use(cors({ origin: 'https://codyit.vercel.app'}))
 
 
 
+const Post = mongoose.model('Post', {
+  authour: String,
+  time: Date,
+  likes: Number,
+  description: String,
+  tags: [String]
+});
+mongoose.connect(procces.env.MONGO_URL)
+.then(_ => console.log('conneting ... '))
+.catch(err => console.log(err))
 
-
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get('/postes', (req, res) => {
+  res.json({word :'hello frombackend'})
 })
-
-//const Post = mongoose.model('Post', Postschema);
-//mongoose.connect('')
-//.then(_ => console.log('conneting ... '))
-//.catch(err => console.log(err))
-
-//app.get('/postes', (req, res) => {
-//  res.json({word :'hello frombackend'})
-// })
-  // try {
-  //   const posts = await Post.find();
-  //   res.json(posts)
-  // } catch (err) {
-  //   console.log('error #')
-  // }
+  try {
+    const posts = await Post.find();
+    res.json(posts)
+  } catch (err) {
+    console.log('error #')
+  }
   
   app.listen(3000, _ => console.log('hello in console'))
