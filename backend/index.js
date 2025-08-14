4f6698a916510a695b4ffdfd92c159a73506288d
antyscript <antyscript@gmail.com>
2025-08-14 19:25:33 +0100

    the website is working now!

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 const { Schema } = mongoose;
 const cors = require('cors');
-app.use(cors({ origin: 'https://codyit.vercel.app'}))
+app.use(cors({ origin: '*'}))
 
 
 
@@ -14,21 +14,64 @@ const Post = mongoose.model('Post', {
   time: Date,
   likes: Number,
   description: String,
-  tags: [String]
+  tags: [String],
+  pass: String
 });
 mongoose.connect(process.env.MONGO_URL)
 .then(_ => console.log('conneting ... '))
 .catch(err => console.log(err))
 
-app.get('/postes', (req, res) => {
-  console.log('codit get postes ===')
-  res.json({word :'hello frombackend'})
-})
-/*  try {
+app.get('/postes', async (req, res) => {
+ try {
     const posts = await Post.find();
+    console.log('codit get postes ===')
     res.json(posts)
   } catch (err) {
-    console.log('error #')
+    console.log('error #' + err)
+    res.json({msg : 'no posts yet or db error'})
   }
-  */
+})
+
+
+app.post('/form', async (req,res)=>{
+  try{
+  const { hisname, content, tags, pass } = req.body
+  console.log('$$$$$$$$$$$$$$$$$$$$$$$')
+  console.log(`name = ${hisname}`);
+  console.log(`content = ${content}`);
+  console.log(`tags = ${tags}`);
+  console.log('$$$$$$$$$$$$$$$$$$$$$$$');
+  
+  const newPost = new Post({
+    authour: hisname, 
+    time: new Date(), 
+    likes: 0, 
+    description: content, 
+    tags,
+    pass
+  })
+  await newPost.save()
+  
+  res.json({msg: "done !"})
+  } catch (err) {
+    res.json({msg: "failed !"})
+  }
+})
+
+
+// app.patch('/postes/:id', async (req, res)=> {
+//   try{
+//   const likedPost = await
+//   Post.findByIdAndUpdate(req.params.id,
+//   {inc : likes: 1 }, {new: true});
+//   res.json(likedPost)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// })
+
+
+
+
+
   app.listen(3000, _ => console.log('hello in console'))
--- frontend/main.js
+++ frontend/main.js
@@ -1,36 +1,74 @@
 
 let main = document.querySelector('main');
-articles = [];
+let articles = [];
 main.innerHTML = '<span class="msg">fetching content ...</span>';
 
-
- fetch('https://coditbackend.onrender.com/postes').then(res => res.json())
- .then(data => main.innerHTML = JSON.stringify(data))
-// fetch('https://localhost:3000/postes')
-//   .then(res => res.json())
-//   .then(posts => {
-//     posts.map( post =>{
-//       let article = `<article class="post">
-//                       <div class="heed">
-//                         <div class="details">
-//                         By <span class="authour">${post.authour}</span>
-//                         - <span class="time">${post.time}</span>
-//                         </div>
-//                       <div class="stats">
-//                         <i class="fas fa-heart"></i>
-//                         <span class="likes">${post.likes}</span>
-//                       </div>
-//                     </div>
-//                       <hr>
-//                       <div class="post-main">${post.description}</div>
-//                     <div class="post-meta">
-//           ${post.tags.map(tag => `<p class="tags">${tag}</p>`).join('')}
-//                     </div>
-//                   </article>`;
-//           articles.push(article)
-//     });
-//       main.innerHTML = articles.join('')
-//   }).catch(err => {
-//     main.innerHTML = '<span class="msg">Server data fetch error or connection problem</span>';
-//     console.log(err)
-//     })
+// fetch('http://localhost:3000/postes').then(res => res.json())
+// .then(data => { 
+//   let newData = JSON.parse(data)
+   
+//   if (data.msg){
+//     main.innerHTML = `<p>${data.msg}</p>`
+//   } else {
+//   main.innerHTML = JSON.stringify(data)}
+// }
+//   )
+fetch('http://localhost:3000/postes')
+  .then(res => res.json())
+  .then(posts => {
+    posts.map( post =>{
+      
+const formatted = post.time? new Date(post.time).toISOString().slice(0, 10).replace(/-/g, '/') : 'undefined';
+      
+      let lik = {
+        id: post._id,
+        liked : localStorage.getItem('liked') || false
+      }
+      
+      let article = `<article class="post">
+                      <div class="heed">
+                        <div class="details">
+                        By <span class="authour">${post.authour}</span>
+                        - <span class="time">${formatted}</span>
+                        </div>
+                      <div class="stats">
+       <i class="fas fa-heart" onclick="like(${lik})"></i>
+                       <span class="likes">${post.likes}</span>
+                      </div>
+                    </div>
+                      <hr>
+                      <div class="post-main">${post.description}</div>
+                    <div class="post-meta">
+          ${post.tags.map(tag => `<p class="tags">${tag}</p>`).join('')}
+                    </div>
+                  </article>`;
+          articles.push(article)
+    });
+      main.innerHTML = articles.join('')
+  }).catch(err => {
+    main.innerHTML = '<span class="msg">Server data fetch error or connection problem</span>';
+    console.log(err)
+    })
+    
+    
+    
+    // function like({id, liked}){
+    //   if (!liked){
+    //   fetch(`http://localhost:3000/postes/${id}`, {
+    //     method: 'PATCH'
+    //   }).then(res => res.json())
+    //     .then(data => {
+    //       console.log(data);
+    //       liked = true;
+    //       localStorage.setItem('liked', liked)
+    //       })
+    //   }
+    // }
+    
+    // function deletePost(id){
+    //   fetch(`http://localhost:3000/delete/${id}`, {
+    //     method: 'DELETE',
+    //   })
+    //   .then(res => res.json())
+    //   .then(data => console.log(data))
+    // }
